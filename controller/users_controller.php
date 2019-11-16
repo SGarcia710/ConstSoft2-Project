45e1775329dcc1d335d0df90bb46cdbf4976db13
Sebastián <sgarcia710@outlook.com>
2019-11-16 02:17:01 -0500

    comas

--- controller/users_controller.php
+++ controller/users_controller.php
@@ -1,6 +1,6 @@
 <?php
-        echo "<h1>" .$_POST['username'];. "</h1> ";
-        echo "<h1>" .$_POST['password'];. "</h1>";      
+        echo "<h1>" .$_POST['username']. "</h1> ";
+        echo "<h1>" .$_POST['password']. "</h1>";      
 
 	// include("../model/users_model.php");
 	// $users= new users_model();
