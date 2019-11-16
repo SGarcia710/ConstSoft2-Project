be14cfec599b55ba225b9a79d0a104a2e50317b2
Sebastián <sgarcia710@outlook.com>
2019-11-16 02:23:43 -0500

    slash

--- controller/users_controller.php
+++ controller/users_controller.php
@@ -3,8 +3,8 @@
         echo "<h1>" .$_POST['password']. "</h1>";      
 
 	include("../model/users_model.php");
-	$users= new users_model();
-  $usersMatrix = $users->get_users();
+	$user= new users_model();
+  $usersMatrix = $user->get_users();
   foreach($usersMatrix as $credentials){
     echo "<h1>" .$credentials['username_usr']. "</h1> ";
     echo "<h1>" .$credentials['password']. "</h1> ";
