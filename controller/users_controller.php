10d07032c45d93d72159a4bede6d13f691923a62
Sebastián <sgarcia710@outlook.com>
2019-11-16 02:16:02 -0500

    ejej

--- controller/users_controller.php
+++ controller/users_controller.php
@@ -1,6 +1,9 @@
 <?php
-	include("../model/users_model.php");
-	$users= new users_model();
-	$usersMatrix = $users->get_users();
-	include("../view/index.php");
+        echo "<h1>" .$_POST['username'];. "</h1> ";
+        echo "<h1>" .$_POST['password'];. "</h1>";      
+
+	// include("../model/users_model.php");
+	// $users= new users_model();
+	// $usersMatrix = $users->get_users();
+	// include("../view/index.php");
   ?>
