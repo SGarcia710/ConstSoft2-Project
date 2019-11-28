d878f49e1fe96635c536f0d4095ecb2b5a357e6b
Sebastián <sgarcia710@outlook.com>
2019-11-28 11:14:06 -0500

    Signup redirecting when you're logged on

--- view/signup.php
+++ view/signup.php
@@ -1,111 +1,68 @@
+<?php
+session_start();
+error_reporting(0);
+$varsesion = $_SESSION['username'];
+if ($varsesion != null || $varsesion != '') {
+  header("Location: ./index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Registro</title>
-    <link rel="stylesheet" href="../styles/cssregister.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-      crossorigin="anonymous"
-    ></script>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-10 col-xl-9 mx-auto">
-          <div class="card card-signin flex-row my-5">
-            <div class="card-img-left d-none d-md-flex"></div>
-            <div class="card-body">
-              <h5 class="card-title text-center">Creación de una cuenta</h5>
-              <form action="./check_signup.php" method="post" class="form-signin">
-                <div class="form-label-group">
-                  <input
-                    type="text"
-                    id="inputUserame"
-                    class="form-control"
-                    placeholder="Username"
-                    required
-                    autofocus
-                    name="username"
-                  />
-                  <label for="inputUserame">Nombre de Usuario</label>
-                </div>
 
-                <div class="form-label-group">
-                  <input
-                    type="email"
-                    id="inputEmail"
-                    class="form-control"
-                    placeholder="Email address"
-                    required
-                    name="email"
-                  />
-                  <label for="inputEmail">Email</label>
-                </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Registro</title>
+  <link rel="stylesheet" href="../styles/cssregister.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</head>
 
-                <hr />
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card card-signin flex-row my-5">
+          <div class="card-img-left d-none d-md-flex"></div>
+          <div class="card-body">
+            <h5 class="card-title text-center">Creación de una cuenta</h5>
+            <form action="./check_signup.php" method="post" class="form-signin">
+              <div class="form-label-group">
+                <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus name="username" />
+                <label for="inputUserame">Nombre de Usuario</label>
+              </div>
 
-                <div class="form-label-group">
-                  <input
-                    type="password"
-                    id="inputPassword"
-                    class="form-control"
-                    placeholder="Password"
-                    name="password1"
-                    required
-                  />
-                  <label for="inputPassword">Contraseña</label>
-                </div>
+              <div class="form-label-group">
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required name="email" />
+                <label for="inputEmail">Email</label>
+              </div>
 
-                <div class="form-label-group">
-                  <input
-                    type="password"
-                    id="inputConfirmPassword"
-                    class="form-control"
-                    placeholder="Password"
-                    name="password2"
-                    required
-                  />
-                  <label for="inputConfirmPassword">Confirmar Contraseña</label>
-                </div>
+              <hr />
 
-                <button
-                  class="btn btn-lg btn-primary btn-block text-uppercase"
-                  type="submit" name="send"
-                >
-                  Crear cuenta
-                </button>
-                <a class="d-block text-center mt-2 small" href="../index.php"
-                  >Iniciar sesión</a
-                >
-              </form>
-            </div>
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password1" required />
+                <label for="inputPassword">Contraseña</label>
+              </div>
+
+              <div class="form-label-group">
+                <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Password" name="password2" required />
+                <label for="inputConfirmPassword">Confirmar Contraseña</label>
+              </div>
+
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="send">
+                Crear cuenta
+              </button>
+              <a class="d-block text-center mt-2 small" href="../index.php">Iniciar sesión</a>
+            </form>
           </div>
         </div>
       </div>
     </div>
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-      integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+</body>
+
 </html>
